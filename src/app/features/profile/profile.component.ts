c5febdd7606bcf6eaa81f0704295ccfcaa1311b1
Colin Peterson <colin9416@gmail.com>
2024-02-18 17:34:00 -0500

    User profile page

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from '@auth0/auth0-angular';
 import { UserProfileModel } from '@app/core';
 
 @Component({
@@ -8,17 +9,10 @@ import { UserProfileModel } from '@app/core';
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
-  user$ = of({
-    nickname: 'Customer',
-    name: 'One Customer',
-    picture: 'https://cdn.auth0.com/blog/hello-auth0/auth0-user.png',
-    updated_at: '2021-05-04T21:33:09.415Z',
-    email: 'customer@example.com',
-    email_verified: false,
-    sub: 'auth0|12345678901234567890',
-  } as UserProfileModel);
-
-  title = 'User Profile Object';
-
+  title = 'Decoded ID Token';
+  
+  user$ = this.authService.user$;
   code$ = this.user$.pipe(map((user) => JSON.stringify(user, null, 2)));
+
+  constructor(private authService: AuthService) {}
 }
